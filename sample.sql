e4b0722e75591875a37be66ca9d4889d4830e2a0
tomo0111 <tohito0430@gmail.com>
2018-06-24 00:15:34 +0900

    refactor sql

--- sample.sql
+++ sample.sql
@@ -1,38 +1,15 @@
--- name: create-auth-server
-CREATE DATABASE auth_server;
+-- name: sample
+CREATE DATABASE sample;
 
--- name: use-auth-server
-USE auth_server;
+-- name: sample
+USE sample;
 
--- name: drop-tokens
-DROP TABLE IF EXISTS tokens;
-
--- name: drop-users
-DROP TABLE IF EXISTS users;
+-- name: test
+DROP TABLE IF EXISTS test;
 
 -- name: create-users
-CREATE TABLE users (
+CREATE TABLE test (
   id int(11) NOT NULL AUTO_INCREMENT,
-  uuid varchar(128) NOT NULL,
-  username varchar(128) NOT NULL,
-  email varchar(128) NOT NULL,
-  password varchar(128) NOT NULL,
   created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (id),
-  UNIQUE (email)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- name: create-tokens
-CREATE TABLE tokens (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  token_type varchar(128) NOT NULL,
-  token varchar(512) NOT NULL,
-  refresh_token varchar(512) NOT NULL,
-  user_id int(11) NOT NULL,
-  expires_at datetime,
-  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (id),
-  UNIQUE (token),
-  FOREIGN KEY (user_id) REFERENCES users(id)
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
