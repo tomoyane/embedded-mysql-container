15a48ddd9117dc9384b6cc1d97855efc229fc58a
tomo0111 <tohito0430@gmail.com>
2018-06-30 22:24:20 +0900

    fix open/close method

--- embedded_mysql_config.go
+++ embedded_mysql_config.go
@@ -7,19 +7,21 @@ type EmbeddedMysqlConfig struct {
 
 var mysqlResolverImpl = EmbeddedMysqlHandlerImpl{}.NewMysqlResolver()
 var db *sql.DB
+var driverName = "mysql"
+var dataSourceName = "root:root@tcp(127.0.0.1:33306)/"
 
-func (c EmbeddedMysqlConfig) Open() {
-	db, openErr := sql.Open("mysql", "root:root@tcp(127.0.0.1:33306)/")
+func (c EmbeddedMysqlConfig) ConnectMysql() {
+	db, openErr := sql.Open(driverName, dataSourceName)
 	if openErr != nil {
 		panic(openErr)
 	}
 	defer db.Close()
 }
 
-func (c EmbeddedMysqlConfig) Close() {
+func (c EmbeddedMysqlConfig) CloseMysql() {
 	db.Close()
 }
 
 func (c EmbeddedMysqlConfig) addSchema(databaseName string)  {
 	mysqlResolverImpl.Insert("CREATE DATABASE " + databaseName)
-}
+}	
