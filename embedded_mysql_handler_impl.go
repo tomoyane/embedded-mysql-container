911e5380339ad8bc3393f6f8b630f5bc5c5717db
tomo0111 <tohito0430@gmail.com>
2018-06-30 22:24:05 +0900

    fix method processing

--- embedded_mysql_handler_impl.go
+++ embedded_mysql_handler_impl.go
@@ -6,22 +6,50 @@ import (
 
 type EmbeddedMysqlHandlerImpl struct {}
 
+var errorHandler = Error{}
+
 func (m EmbeddedMysqlHandlerImpl) NewMysqlResolver() EmbeddedMysqlHandler {
 	return &EmbeddedMysqlHandlerImpl{}
 }
 
 func (m EmbeddedMysqlHandlerImpl) CreateDatabase(query string) {
-
+	_, queryErr := db.Exec(query)
+	if queryErr != nil {
+		errorHandler.ErrorMessage(
+			"mysql create database failed.",
+			queryErr,
+		)
+	}
 }
 
 func (m EmbeddedMysqlHandlerImpl) CreateTable(query string)  {
+	_, queryErr := db.Exec(query)
+	if queryErr != nil {
+		errorHandler.ErrorMessage(
+			"mysql create table failed.",
+			queryErr,
+		)
+	}
 }
 
 func (m EmbeddedMysqlHandlerImpl) DropTable(query string) {
-
+	_, queryErr := db.Exec(query)
+	if queryErr != nil {
+		errorHandler.ErrorMessage(
+			"mysql drop table failed.",
+			queryErr,
+		)
+	}
 }
 
 func (m EmbeddedMysqlHandlerImpl) Insert(query string) {
+	_, queryErr := db.Exec(query)
+	if queryErr != nil {
+		errorHandler.ErrorMessage(
+			"mysql create database failed.",
+			queryErr,
+		)
+	}
 }
 
 func (m EmbeddedMysqlHandlerImpl) Update(query string) {
