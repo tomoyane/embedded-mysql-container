e8c3a47527066f49072930f45c53fddea5be1e2b
tomo0111 <tohito0430@gmail.com>
2018-07-08 00:40:34 +0900

    refactor main function

--- main.go
+++ main.go
@@ -1,23 +1,17 @@
 package main
 
-var containerDaemon = ContainerDaemon{}
-
 func main() {
-	containerDaemon.InitDocker()
+	containerDaemon := ContainerDaemonImpl{}.NewContainerDaemonImpl()
 
+	containerDaemon.InitDocker()
 	containerDaemon.PullImage("docker.io/library/mysql:5.7")
 
-	var containerId = containerDaemon.BuildImage(
+	containerId := containerDaemon.BuildImage(
 		"mysql:5.7",
 		"embedded_mysql",
 	)
 
 	containerDaemon.StartContainer(containerId)
-
-	//containerDaemon.WaitForContainer(containerId)
-
 	containerDaemon.SetupLogOfContainer(containerId)
-
-	mysqlConfig := EmbeddedMysqlConfig{}
-	mysqlConfig.addSchema("sample")
+	containerDaemon.StopContainer(containerId)
 }
