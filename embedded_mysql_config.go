0ed6942c3a33ba9ab301dd10fbdfba6b7daf15d4
tomo0111 <tohito0430@gmail.com>
2018-07-13 11:19:41 +0900

    add struct

--- embedded_mysql_config.go
+++ embedded_mysql_config.go
@@ -4,29 +4,44 @@ import (
 	"database/sql"
 )
 
+type EmbeddedMysqlConfig struct {
+	errorHandler *Error
+}
+
 var (
 	driverName = "mysql"
 	dataSourceName = "root:root@tcp(127.0.0.1:33306)/"
-	db *sql.DB
+	dataBaseName string
+	Db *sql.DB
 )
 
-func ConnectMysql(dbName string) {
-	dataSourceName = dataSourceName + dbName
-	db, openErr := sql.Open(driverName, dataSourceName)
-	if openErr != nil {
-		panic(openErr)
+func (c EmbeddedMysqlConfig) ConnectMysql(dbName string) {
+	if dataBaseName == "" {
+		c.errorHandler.ErrorMessage("Need to set database name", nil)
+		panic(nil)
+	}
+
+	db, err := sql.Open(driverName, dataSourceName + dataBaseName)
+	if err != nil {
+		panic(err)
 	}
 	defer db.Close()
+
+	Db = db
 }
 
-func CloseMysql() {
-	db.Close()
+func (c EmbeddedMysqlConfig) CloseMysql() {
+	Db.Close()
 }
 
-func GetDriverName() string {
+func (c EmbeddedMysqlConfig) SetDbName(dbName string) {
+	dataBaseName = dbName
+}
+
+func (c EmbeddedMysqlConfig) GetDriverName() string {
 	return driverName
 }
 
-func GetDataSourceName() string {
-	return dataSourceName
+func (c EmbeddedMysqlConfig) GetDataSourceName() string {
+	return dataSourceName + dataBaseName
 }
