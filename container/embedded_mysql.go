c6259d80933ef7c29bc5be5f8c91eec04140211b
tomoyane <tohito0430@gmail.com>
2018-08-12 11:50:29 +0900

    add embedded mysql method

--- container/embedded_mysql.go
+++ container/embedded_mysql.go
@@ -1,21 +1,7 @@
 package container
 
-import (
-	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
-	"time"
-)
+type MysqlConfig interface {
+	AddSchema(dbName string)
 
-func AddSchema(name string) {
-
-	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:33306)/")
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	time.Sleep(10000)
-	db.Exec("CREATE DATABASE " + name)
-
-	db.Close()
+	CreateTable(query string)
 }
